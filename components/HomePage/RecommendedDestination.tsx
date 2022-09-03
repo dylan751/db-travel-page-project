c924cba5cb4b213cb2695c4eceb91efe0456dc5d
Nguyen Duong <muoi07052001@gmail.com>
2022-09-03 21:49:20 +0700

    Update UI

--- components/HomePage/RecommendedDestination.tsx
+++ components/HomePage/RecommendedDestination.tsx
@@ -1,64 +1,84 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-import { ArrowSmRightIcon } from "@heroicons/react/solid";
+import { ArrowSmRightIcon } from '@heroicons/react/solid';
 
-import classes from "./RecommendedDestination.module.css";
+import classes from './RecommendedDestination.module.css';
 
 const RecommendedDestination = () => {
   return (
-    <section className={classes["recommended-destination"]}>
-      <div className={classes["recommended-destination-container"]}>
+    <section className={classes['recommended-destination']}>
+      <div className={classes['recommended-destination-container']}>
         <span>All the</span>
         <h2>Best destinations</h2>
-        <ul className={classes["destination"]}>
+        <ul className={classes['destination']}>
           <li>
-            <img src="/images/destination/toronto.jpg" alt="Australia" />
-            <div className={classes["description"]}>
+            <img
+              src="/images/destination/toronto.jpg"
+              alt="Australia"
+              loading="lazy"
+            />
+            <div className={classes['description']}>
               <Link href="/destination">
-                <a className={classes["name"]}>Thành phố Toronto</a>
+                <a className={classes['name']}>Thành phố Toronto</a>
               </Link>
               <Link href="/destination">
-                <a className={classes["explore"]}>
-                  Explore <ArrowSmRightIcon className="w-5 h-5 inline-block text-clr_jaffa" />
+                <a className={classes['explore']}>
+                  Explore{' '}
+                  <ArrowSmRightIcon className="w-5 h-5 inline-block text-clr_jaffa" />
                 </a>
               </Link>
             </div>
           </li>
           <li>
-            <img src="/images/destination/istanbul.jpg" alt="Newzealand" />
-            <div className={classes["description"]}>
+            <img
+              src="/images/destination/istanbul.jpg"
+              alt="Newzealand"
+              loading="lazy"
+            />
+            <div className={classes['description']}>
               <Link href="/destination">
-                <a className={classes["name"]}>Istanbul</a>
+                <a className={classes['name']}>Istanbul</a>
               </Link>
               <Link href="/destination">
-                <a className={classes["explore"]}>
-                  Explore <ArrowSmRightIcon className="w-5 h-5 inline-block text-clr_jaffa" />
+                <a className={classes['explore']}>
+                  Explore{' '}
+                  <ArrowSmRightIcon className="w-5 h-5 inline-block text-clr_jaffa" />
                 </a>
               </Link>
             </div>
           </li>
           <li>
-            <img src="/images/destination/pho-co.jpg" alt="Egypt" />
-            <div className={classes["description"]}>
+            <img
+              src="/images/destination/pho-co.jpg"
+              alt="Egypt"
+              loading="lazy"
+            />
+            <div className={classes['description']}>
               <Link href="/destination">
-                <a className={classes["name"]}>Phố cổ Hà Nội</a>
+                <a className={classes['name']}>Phố cổ Hà Nội</a>
               </Link>
               <Link href="/destination">
-                <a className={classes["explore"]}>
-                  Explore <ArrowSmRightIcon className="w-5 h-5 inline-block text-clr_jaffa" />
+                <a className={classes['explore']}>
+                  Explore{' '}
+                  <ArrowSmRightIcon className="w-5 h-5 inline-block text-clr_jaffa" />
                 </a>
               </Link>
             </div>
           </li>
           <li>
-            <img src="/images/destination/mount-fuji.jpg" alt="Africa" />
-            <div className={classes["description"]}>
+            <img
+              src="/images/destination/mount-fuji.jpg"
+              alt="Africa"
+              loading="lazy"
+            />
+            <div className={classes['description']}>
               <Link href="/destination">
-                <a className={classes["name"]}>Núi Phú Sỹ</a>
+                <a className={classes['name']}>Núi Phú Sỹ</a>
               </Link>
               <Link href="/destination">
-                <a className={classes["explore"]}>
-                  Explore <ArrowSmRightIcon className="w-5 h-5 inline-block text-clr_jaffa" />
+                <a className={classes['explore']}>
+                  Explore{' '}
+                  <ArrowSmRightIcon className="w-5 h-5 inline-block text-clr_jaffa" />
                 </a>
               </Link>
             </div>
@@ -67,6 +87,6 @@ const RecommendedDestination = () => {
       </div>
     </section>
   );
-}
+};
 
 export default RecommendedDestination;
